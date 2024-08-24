b4776e06066472e54b9bdfdb5c1b2c7a54001830
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-24 20:00:44 -0300

    Making the counter i a static property to debugging propouses

--- src/br/com/terceiro/debugging/Deb.java
+++ src/br/com/terceiro/debugging/Deb.java
@@ -1,8 +1,9 @@
 package br.com.terceiro.debugging;
 
 public class Deb {
+	public static int i = 0;
 	public static void main(String[] args) {
-		for (int i=0 ; i<5 ; i++) {
+		for (i=0 ; i<5 ; i++) {
 			System.out.println(i);
 		}
 	}
